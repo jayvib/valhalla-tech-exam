e013bc4a0cb32a68c9a7719b6ae4bf000942d779
Jayson Vibandor <danv@isell.com.au>
2020-03-21 12:00:59 +0100

    added unit test for the problem 3

---
+++ 03/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestBubbleSortInt(t *testing.T) {
+  input := []int{3, 2, 5, 8, 1, 2}
+  want := []int{1, 2, 2, 3, 5, 8}
+  bubbleSortInt(input)
+  assert.Equal(t, want, input)
+}
