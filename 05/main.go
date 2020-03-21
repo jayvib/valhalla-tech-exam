8788ebfd80265ea6ef99d0f55a6278e43aa98443
Jayson Vibandor <danv@isell.com.au>
2020-03-21 09:07:23 +0100

    implemented item 05

--- 05/main.go
+++ 05/main.go
@@ -7,3 +7,40 @@ package main
 func main() {
 }
 
+func IsSumOfTwoItemExistsFrom(sum int, items []int) bool {
+  // Steps:
+  // Sort first the array of integer
+  // Get the sum of the left and right value of the array and compare it to sum.
+  bubbleSortInt(items)
+  left := 0
+  right := len(items)-1
+
+  for left < right {
+    if (items[left] + items[right]) == sum {
+      return true
+    } else if (items[left] + items[right]) < sum {
+      left++
+    } else {
+      right--
+    }
+  }
+
+  return false
+}
+
+func bubbleSortInt(ints []int) {
+  unsortedUntilIndex := len(ints) - 1
+  isSorted := false
+  for !isSorted {
+    isSorted = true // assume that this pass through is already sorted
+    // check if the items are really sorted
+    for i := 0; i < unsortedUntilIndex; i++ {
+      if ints[i] > ints[i+1] {
+        // swap
+        ints[i], ints[i+1] = ints[i+1], ints[i]
+        isSorted = false
+      }
+    }
+    unsortedUntilIndex--
+  }
+}
---
+++ 05/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "github.com/stretchr/testify/assert"
+  "testing"
+)
+
+func TestIsSumOfTwoItemExistsFrom(t *testing.T) {
+
+  cases := []struct{
+    name string
+    input    []int
+    sum      int
+    isExists bool
+  }{
+    {
+     name: "Not Exists",
+     input:    []int{1, 2, 3, 4},
+     sum:      8,
+     isExists: false,
+    } ,
+    {
+      name: "Exists",
+      input:    []int{4, 2, 4, 1},
+      sum:      8,
+      isExists: true,
+    } ,
+  }
+
+  for _, c := range cases {
+    t.Run(c.name, func(t *testing.T){
+      got := IsSumOfTwoItemExistsFrom(c.sum, c.input)
+      assert.Equal(t, c.isExists, got)
+    })
+  }
+}
